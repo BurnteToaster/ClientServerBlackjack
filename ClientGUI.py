8711284b7d1e8d5465d4a71bdfe81418fa1198d3
Stixels <89710831+Stixels@users.noreply.github.com>
2021-12-05 23:04:09 -0500

    Ace value fixed

--- ClientGUI.py
+++ ClientGUI.py
@@ -26,6 +26,9 @@ responseLabel.grid(row=2, column=0)
 
 # add hand label when game starts
 
+# popup window for ace value
+
+
 # start button function
 def start():
     clientSocket.send("start round\n".encode())
@@ -46,18 +49,45 @@ startButton.grid(row=97, column=0)
 # gets the value of a card from the server
 # jack king queen are all worth 10
 def cardValue(card):
+    global value
     value = 0
     if card[:2] == "10" or card[:2] == "11" or card[:2] == "12" or card[:2] == "13":
         value = 10
-    #We need to fix this somehow
-    elif card[0] == "1":
-        value = str(input("Ace! Enter value 1 or 11: "))
+    # Choose Ace value here
+    elif card[0] == "1":    
+        # Set the value for the ace
+        def setValue(num):
+            global value
+            if num == 1:
+                value = 1
+            else:
+                value = 11 
+            popup.destroy()
+    
+        popup = Toplevel(root)
+        popup.title("Ace Value")
+
+        l = Label(popup, text="Would you like the Ace to be a 1 or 11?")
+        l.grid(row=0, column=0)
+
+        b1 = Button(popup, text="1", command=lambda: setValue(1))
+        b1.grid(row=1, column=0)
+        b1.config(state=NORMAL)
+
+        b2 = Button(popup, text="11", command=lambda: setValue(11))
+        b2.grid(row=2, column=0)
+        b2.config(state=NORMAL)
+
+        if 11 + handValue > 21:
+            b2.config(state=DISABLED)
+
+        root.wait_window(popup)
     else:
         value = int(card[0])
-    
+
     return value
 
-# formatting to display cards
+# formatting to display cards in GUI
 def cardDisplay(card):
     if card[:2] == "10":
         return "10" + card[2:]
@@ -67,6 +97,8 @@ def cardDisplay(card):
         return "Queen" + card[2:]
     elif card[:2] == "13":
         return "King" + card[2:]
+    elif card[0] == "1":
+        return "Ace" + card[1:]
     else:
         return card[0] + " " + card[2:]
 
@@ -95,7 +127,6 @@ def hit():
         statusLabel.config(text="Waiting for other player...")
         hitButton.config(state=DISABLED)
         standButton.config(state=DISABLED)
-        time.sleep(1.5)
         clientSocket.send("game over\n".encode())
         clientSocket.send((str(handValue)+"\n").encode())
         print(hand)
